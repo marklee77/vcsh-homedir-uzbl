c44fc724c5e946297ddb62b0f895620a417fe605
Mark Stillwell <mark@stillwell.me>
2015-09-14 23:04:36 +0100

    autocommit

--- .local/share/uzbl/scripts/formfiller.py
+++ .local/share/uzbl/scripts/formfiller.py
@@ -7,6 +7,7 @@ from urlparse import urlparse
 from xdg.BaseDirectory import xdg_data_home
 
 import gnupg
+import yaml
 
 
 uzbl_forms_dir = os.path.join(xdg_data_home, 'uzbl', 'forms')
@@ -19,20 +20,24 @@ def main(argv=None):
     except OSError:
         os.chmod(uzbl_forms_dir, 0700)
 
-    data = ""
+    response = ''
     try:
         s = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
         s.connect(os.environ.get('UZBL_SOCKET', None))
         s.sendall('js uzbl.formfiller.dump();\n')
-        data = s.recv(16384)
+        response = s.recv(16384)
         s.close()
     except:
         pass
 
+    message, json_data = response.split('\n', 1)
+    data = yaml.load(json_data)
+    yaml_data = yaml.dump(data)
+
     hostname = urlparse(os.getenv('UZBL_URI', 'http://undefined')).hostname
 
     gpg = gnupg.GPG()
-    encrypted_data = str(gpg.encrypt(data, 'mark@stillwell.me'))
+    encrypted_data = str(gpg.encrypt(yaml_data, 'mark@stillwell.me'))
 
     try:
         formfile = open(os.path.join(uzbl_forms_dir, hostname + '.asc'), 'w')
