68eeda1343a75de6991f562b6436aa31944c193a
Mark Stillwell <mark@stillwell.me>
2015-09-15 11:19:55 +0100

    autocommit

--- .local/share/uzbl/scripts/formfiller.py
+++ .local/share/uzbl/scripts/formfiller.py
@@ -4,7 +4,6 @@
 #   - configuration file
 #   - match uri?
 import os
-import socket
 import sys
 from urlparse import urlparse
 
@@ -26,21 +25,38 @@ def store_data():
     pass
 
 
-def send_javascript():
+def send_javascript(script):
+    import socket
+    response = ''
+    try:
+        s = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+        s.connect(os.environ.get('UZBL_SOCKET', None))
+        s.sendall(script + '\n')
+        response = s.recv(16384)
+        s.close()
+    except:
+        pass
+
+    _, json = response.split('\n', 1)
+
+    return json.loads(json)
+
+def dump_window_form_data():
+        s.sendall('js uzbl.formfiller.dump();\n')
     pass
 
 
-def dump_window_form_data():
+def update_window_form_data():
     pass
 
 
 def main(argv=None):
 
-    try:
-        os.makedirs(uzbl_forms_dir, 0700)
-    except OSError:
-        os.chmod(uzbl_forms_dir, 0700)
+    # get data from uzbl window
 
+    dump_data = yaml.load(json_dump_data)
+
+    # parse uri for information needed to load/update/save site data
     uri = os.getenv('UZBL_URI', 'noproto://undefined')
     urlparse_result = urlparse(uri)
     hostname = urlparse_result.hostname
@@ -48,6 +64,7 @@ def main(argv=None):
     if path is None or path == '':
         path = '/'
 
+    # update site data
     data = {}
     try:
         formfile = open(os.path.join(uzbl_forms_dir, hostname + '.yml.asc'),
@@ -55,25 +72,17 @@ def main(argv=None):
         data = yaml.load(str(gpg.decrypt_file(formfile)))
     except:
         pass
-
-    response = ''
-    try:
-        s = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-        s.connect(os.environ.get('UZBL_SOCKET', None))
-        s.sendall('js uzbl.formfiller.dump();\n')
-        response = s.recv(16384)
-        s.close()
-    except:
-        pass
-
-    message, json_dump_data = response.split('\n', 1)
-    dump_data = yaml.load(json_dump_data)
-
     data[path] = dump_data
 
+    # save site form data
     yaml_data = yaml.dump(data, width=79, indent=2, default_flow_style=False,
                           explicit_start=True)
     encrypted_data = str(gpg.encrypt(yaml_data, 'mark@stillwell.me'))
+
+    try:
+        os.makedirs(uzbl_forms_dir, 0700)
+    except OSError:
+        os.chmod(uzbl_forms_dir, 0700)
 
     try:
         formfile = open(os.path.join(uzbl_forms_dir, hostname + '.yml.asc'),
